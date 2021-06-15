27be948e22b7791b48199106a01ee382eda7252d
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-06-15 15:09:36 -0300

    Entrega do exercício de operadores.

---
+++ semana1/operadores/index.js
@@ -0,0 +1,70 @@
+/* ------------------Exercícios de interpretação de código------------------//
+
+  1) a. false
+     b. true
+     c. true
+     d. boolean true
+
+
+  2) Não será impresso nada, porque o prompt só reconhece strings e o colega não colocou uma função específica que faça o prompt reconhecer números. 
+
+  3) O colega teria que alterar os valores das duas variáveis para: 
+
+  - let primeiroNumero = Number(prompt("Digite um numero!"))
+  - let segundoNumero = NUmber(prompt("Digite outro numero!"))
+
+*/
+
+/*
+  // ------------------Exercícios de escrita de código------------------//
+  
+  * Exercício 1
+
+const userAge = Number(prompt("Qual a sua idade?"))
+const bestFriendAge = Number(prompt("Qual é a idade do seu melhor amigo(a)?"))
+
+const ageDifference = userAge - bestFriendAge
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", userAge >= bestFriendAge)
+console.log("A diferença de idade é:", ageDifference)
+
+*/
+
+
+/* 
+  
+  * Exercício 2
+
+const userNumber = Number(prompt("Insira um número par."))
+const restOfDivision = 2 
+
+
+console.log("O resto da divisão é:", userNumber % restOfDivision) // Qualquer número par retorna 0 e qualquer número ímpar retorna 1.
+
+*/
+
+
+/*
+  
+ * Exercício 3
+
+const userAge =  Number(prompt("Qual é a sua idade?"))
+
+console.log("Sua idade em meses é:", userAge * 12)
+console.log("Sua idade em dias é:", userAge * 365)
+console.log("Sua idade em horas é:", userAge * 24 * 365)
+
+*/
+
+/*
+ 
+  * Exercício 4
+
+const firstNumber = Number(prompt("Digite um número."))
+const secondNumber = Number(prompt("Digite outro número!"))
+
+console.log("O primeiro número é maior que o segundo?", firstNumber > secondNumber)
+console.log("O primeiro número é igual ao segundo?", firstNumber === secondNumber)
+console.log("O segundo número é divisível pelo primeiro?", secondNumber % firstNumber === 0)
+
+*/
