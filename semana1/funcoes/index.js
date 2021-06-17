e05c2def7f46e2780e2778ae6134b4274abf664c
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-06-17 16:32:33 -0300

    Entrega dos exercícios sobre funções.

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,75 @@
+/* ----------------------Exercícios de interpretação de código------------------------
+
+1) a - Será impresso o retorno de 10 e 50.
+   b - O console apareceria vazio, porque o console foi removido. Nesse caso, restaria apenas a chamada da função.
+
+2) a - É solicitado que o usuário insira um texto, com a resposta dele a função irá transformar o texto em minúsculo e determinará se contém "cenoura". Se conter "cenoura", o retorno será true e se não conter, false. A resposta do usuário será impressa no console.
+
+   b - I. eu gosto de cenoura true
+       II. cenoura é bom pra vista true
+       III. cenouras crescem na terra true
+*/
+
+//-------------------------Exercícios de escrita de código--------------------//
+
+/* Questão 1) a:
+const information = () => {
+    console.log("Eu sou Ivan, tenho 22 anos, moro em Belo Horizonte e sou estudante.")
+}
+
+information()
+
+// b:
+const userInformation = (name, age, city, profession) => {
+    console.log(`Eu sou ${name}, tenho ${age} anos, moro em ${city} e sou ${profession}.`)
+}
+
+userInformation("Klein", 22, "Londres", "estudante")
+
+*/
+
+//  2) a:
+// const addTwoNumbers = (number1, number2) => {
+//     const sum = number1 + number2
+//     return sum
+// }
+
+// const sumResult = addTwoNumbers(250, 100)
+// console.log("Resultado da soma:", sumResult)
+
+//-------------------------------------------------------
+
+//b:
+// const calculateNumbers = (number1, number2) => {
+//     const calculateResult = number1 >= number2
+//     return calculateResult
+// }
+
+// const result = calculateNumbers(50, 25)
+// console.log("É true ou false?", result)
+
+//---------------------------------------------------------
+
+// c:
+// const checkOddAndEven = (number) => {
+//     return (number % 2 == 0)
+    
+// }
+
+// const checkNumber = checkOddAndEven(20)
+// console.log(checkNumber)
+
+
+// d:
+
+// const printMessage = (message = "Olá, mundo!") => {
+//     return message
+// }
+
+// const messageSize = printMessage()
+// console.log(messageSize)
+
+// --------------------------------------------------
+
+// Questão 3
+
