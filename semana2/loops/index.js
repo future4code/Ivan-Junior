bacf4352d8d2074547a2ffb380341a67063a70c9
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-06-23 16:36:21 -0300

    Entrega do exercício da tarde - Aula 09.

---
+++ semana2/loops/index.js
@@ -0,0 +1,16 @@
+/* ----------------Exercícios de interpretação de código---------------
+
+1) Será impresso o valor das repições 5 vezes e o código será interrompido quando a condição for cumprida. O resultado será de: 10
+
+2) a - Será impresso os números maiores que 18 que são: 19, 21, 23, 25, 27, 30.
+
+b - Sim, mas eu teria que alterar o numeros por lista.length no if.
+
+3 - *
+    **
+    ***
+    ****
+
+
+*/
+
