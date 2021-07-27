33f99b30a5717b53c2b4db28b5bf7b02a2983f7f
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-07-27 16:12:39 -0300

    Entrega dos exercícios da aula 25 - Integrações de API

---
+++ semana7/labenusers/labenusers/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Register } from './components/Register'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Register />
+      </div>
+    );
+  }
+}
