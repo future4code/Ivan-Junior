1f05c48df012f4e662def9554d90b237e08df37c
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-08-10 16:58:05 -0300

    Entrega dos exercícios incompletos da aula 32

---
+++ semana9/exercicio-aula32/src/App.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { useState } from 'react'
+import React, {useEffect} from 'react'
+import PokeCard from './components/PokeCard/index'
+import "./styles.css";
+
+
+const App = () => {
+
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState("")
+
+
+  
+  const getPokemons = () => {
+      axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        console.log(response.data.results)
+         setPokeList(response.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+     getPokemons()
+  }, [])
+
+  const changePokeName = event => {
+    console.log(event.target.value)
+    setPokeName(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <select onChange ={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map((pokemon) => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}></option>
+            )
+          })}
+      </select>
+
+      {pokeName && <PokeCard pokemon={pokeName}/>}
+    </div>
+  )
+}
+
+
+export default App
