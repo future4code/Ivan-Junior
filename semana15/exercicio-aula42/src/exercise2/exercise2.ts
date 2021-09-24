9f278fb039ce81438f75a3ee2689d72269e9edbc
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-09-24 16:49:56 -0300

    I started doing the exercises in class 42

---
+++ semana15/exercicio-aula42/src/exercise2/exercise2.ts
@@ -0,0 +1,25 @@
+
+// const arrayDeNumeros: number[] = [20, 40, 60, 100]
+
+
+
+// function obterEstatisticas(numeros: arrayDeNumeros) {
+
+//     const numerosOrdenados = numeros.sort(
+//         (a, b) => a - b
+//     )
+
+//     let soma = 0
+
+//     for (let num of numeros) {
+//         soma += num
+//     }
+
+//     const estatisticas = {
+//         maior: numerosOrdenados[numeros.length - 1],
+//         menor: numerosOrdenados[0],
+//         media: soma / numeros.length
+//     }
+
+//     return estatisticas
+// }
