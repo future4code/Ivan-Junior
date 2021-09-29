a00de5a7e4fbc4b1466a94b3fd3e05319981edb9
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-09-29 17:02:41 -0300

    I finished exercise 2

--- semana16/exercicio-aula47/aula-knex-express-template/src/index.ts
+++ semana16/exercicio-aula47/aula-knex-express-template/src/index.ts
@@ -26,22 +26,53 @@ import connection from "./connection"
 //   .then(console.log)
 //   .catch(console.log)
 
-// const createActor = async (
-//     id: string,
-//     name: string,
-//     salary: number,
-//     dateOfBirth: Date,
-//     gender: string
-// ): Promise<void> => {
-//     await connection
-//         .insert({
-//             id: id,
-//             name: name,
-//             salary: salary,
-//             birth_date: dateOfBirth,
-//             gender: gender,
-//         })
-//         .into("Actor");
-// };
+const createActor = async (
+    id: string,
+    name: string,
+    salary: number,
+    dateOfBirth: string,
+    gender: string
+): Promise<void> => {
+    await connection
+        .insert({
+            id: id,
+            name: name,
+            salary: salary,
+            birth_date: dateOfBirth,
+            gender: gender,
+        })
+        .into("Actor");
+};
+ 
+  createActor("07", "Keanu Reeves", 5000, "1985-05-12", "male")
 
-// createActor(())
+  const updateActor = async (id: string, salary: number): Promise<any> => {
+    await connection("Actor")
+      .update({
+        salary: salary,
+      })
+      .where("id", id);
+  };
+
+  updateActor("001", 5000000)
+   .then(console.log)
+   .catch(console.log)
+
+// const deleteActor = async (id: string): Promise<void> => {
+//     await connection("Actor")
+//     .delete()
+//     .where("id", id)
+// }
+
+// deleteActor("id")
+
+// const avgSalary = async (gender: string): Promise<any> => {
+//     const result = await connection("Actor")
+//       .avg("salary as average")
+//       .where({ gender });
+  
+//     return result[0].average;
+//   };
+
+//   avgSalary("female")
+
--- semana16/exercicio-aula47/exercicio-aula47.md
+++ semana16/exercicio-aula47/exercicio-aula47.md
@@ -29,3 +29,41 @@ const countActors = async (gender: string): Promise<any> => {
   .catch(console.log)
 };
 ```
+## Exercício 2
+
+### a - 
+```
+const updateActor = async (id: string, salary: number): Promise<any> => {
+//     await connection("Actor")
+//       .update({
+//         salary: salary,
+//       })
+//       .where("id", id);
+//   };
+
+//   updateActor("001", 5000000)
+//    .then(console.log)
+//    .catch(console.log)
+```
+
+### b -
+const deleteActor = async (id: string): Promise<void> => {
+    await connection("Actor")
+    .delete()
+    .where("id", id)
+}
+
+deleteActor("id")
+
+### c - 
+```
+const avgSalary = async (gender: string): Promise<any> => {
+    const result = await connection("Actor")
+      .avg("salary as average")
+      .where({ gender });
+  
+    return result[0].average;
+  };
+
+  avgSalary("female")
+```
